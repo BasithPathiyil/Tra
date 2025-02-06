af8d6b7a92c22e9cb3a13ab6b73f9836d9f44508
BasithPathiyil <basithpathiyil007@gmail.com>
2025-02-06 21:13:53 +0530

    changes

--- services/cronjob.js
+++ services/cronjob.js
@@ -5,6 +5,16 @@ const { sendConsolidationsEmail } = require("./sendmail.service");
 const runScheduledTask = async () => {
   // .slice(0, 16) + "00.000Z";
   console.log("Running scheduled task at:", new Date());
+  const currentIST = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
+  const currentHours = new Date(currentIST).getHours();
+  const currentMinutes = new Date(currentIST).getMinutes();
+  if (currentHours >= 15 && currentMinutes > 10) {
+    console.log("Cron job stopped after 3:10 PM IST.");
+    return;
+  }
+
   const formattedDate = new Date(new Date().getTime() + 5.5 * 3600 * 1000);
 
   let query = {
@@ -23,7 +33,7 @@ const runScheduledTask = async () => {
 
 // Schedule the cron job: Every 5 minutes from 10:00 AM to 3:10 PM, Monday to Saturday
 const startCronJob = () => {
-  cron.schedule("*/5 10-15 * * 1-5", runScheduledTask);
+  cron.schedule("*/5 4-9 * * 1-5", runScheduledTask);
   console.log("Cron job started.");
 };
 
